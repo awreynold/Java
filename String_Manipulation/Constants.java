d9dfcc16c5075b82264788bd60bf2bf9f12c8bad
Arthur Reynolds <33764105+awreynold@users.noreply.github.com>
2019-03-06 19:08:27 -0600

    Update Constants.java

--- String_Manipulation/Constants.java
+++ String_Manipulation/Constants.java
@@ -1,3 +1,42 @@
 public class Constants{
-
+  
+  public static final String PERIOD = ".";
+  public static final String COMMA = ",";
+  public static final String FORWARD_SLASH = "/";
+  public static final String BACK_SLASH = "\\";
+  public static final String QUESTION_MARK = "?";
+  public static final String COLON = ":";
+  public static final String SEMICOLON = ";";
+  public static final String SINGLE_QUOTE = "\'";
+  public static final String DOUBLE_QUOTE = "\"";
+  public static final String ACCENT = "`";
+  public static final String DASH = "-";
+  public static final String UNDERSCORE = "_";
+  public static final String EQUALS = "=";
+  public static final String EXCLAIMATION_POINT = "!";
+  public static final String TILDE = "~";
+  public static final String AT_SYMBOL = "@";
+  public static final String POUND = "#";
+  public static final String DOLLAR_SIGN = "$";
+  public static final String PERCENTAGE = "%";
+  public static final String UP_ARROW = "^";
+  public static final String AMPERSAND = "&";
+  public static final String STAR = "*";
+  public static final String LEFT_PARENTHESIS = "(";
+  public static final String RIGHT_PARENTHESIS = ")";
+  public static final String LEFT_BRACKET = "[";
+  public static final String RIGHT_BRACKET = "]";
+  public static final String LEFT_CURLY_BRACE = "{";
+  public static final String RIGHT_CURLY_BRACE = "}";
+  public static final String LINE = "|";
+  public static final String PLUS = "+";
+  public static final String LESS_THAN = "<";
+  public static final String GREATER_THAN = ">";
+  
+  public static final String[] SYMBOLS = new String[]{PERIOD, COMMA, FORWARD_SLASH, BACK_SLASH, QUESTION_MARK, COLON,
+                                                     SEMICOLON, SINGLE_QUOTE, DOUBLE_QUOTE, ACCENT, DASH, UNDERSCORE,
+                                                     EQUALS, EXCLAIMATION_POINT, TILDE, AT_SYMBOL, POUND, DOLLAR_SIGN,
+                                                     PERCENTAGE, UP_ARROW, AMPERSAND, STAR, LEFT_PARENTHESIS, 
+                                                      RIGHT_PARENTHESIS, LEFT_BRACKET, RIGHT_BRACKET, LEFT_CURLY_BRACE,
+                                                     RIGHT_CURLY_BRACE, LINE, PLUS, LESS_THAN, GREATER_THAN};
 }
